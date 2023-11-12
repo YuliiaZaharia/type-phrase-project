792cd9f5c48c41ac442a1002caf8d18ea71cfb40
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-11-12 16:41:25 +0300

    used methods querySelector(), setTimeout()

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ let i = 0;
 let speed = 100; 
 
 function typePhrase() {
-  
+  if (i < text.length) {
+    document.querySelector("#par").textContent += text.charAt(i);
+    i++; 
+    setTimeout(typePhrase, speed); 
+  }
 }
 
+typePhrase(); 
 
